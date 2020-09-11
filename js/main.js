85263af9621a7e195956f2660966563951b58687
ashwinbalaji0811 <ashwing2981999@gmail.com>
2020-09-11 16:17:59 +0530

    changed for a better theme

--- js/main.js
+++ js/main.js
@@ -1,70 +1,48 @@
-
 (function ($) {
+    // USE STRICT
     "use strict";
 
-
-    /*==================================================================
-    [ Focus Contact2 ]*/
-    $('.input100').each(function(){
-        $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
-                $(this).addClass('has-val');
-            }
-            else {
-                $(this).removeClass('has-val');
-            }
-        })    
-    })
+    $(".form-radio .radio-item").click(function(){
+        //Spot switcher:
+        $(this).parent().find(".radio-item").removeClass("active");
+        $(this).addClass("active");
+    });
   
-  
-    /*==================================================================
-    [ Validate ]*/
-    var name = $('.validate-input input[name="name"]');
-    var email = $('.validate-input input[name="email"]');
-    var message = $('.validate-input textarea[name="message"]');
-
-
-    $('.validate-form').on('submit',function(){
-        var check = true;
-
-        if($(name).val().trim() == ''){
-            showValidate(name);
-            check=false;
-        }
-
-
-        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-            showValidate(email);
-            check=false;
-        }
-
-        if($(message).val().trim() == ''){
-            showValidate(message);
-            check=false;
-        }
-
-        return check;
+    $('#time').parent().append('<ul class="list-item" id="newtime" name="time"></ul>');
+    $('#time option').each(function(){
+        $('#newtime').append('<li value="' + $(this).val() + '">'+$(this).text()+'</li>');
+    });
+    $('#time').remove();
+    $('#newtime').attr('id', 'time');
+    $('#time li').first().addClass('init');
+    $("#time").on("click", ".init", function() {
+        $(this).closest("#time").children('li:not(.init)').toggle();
     });
 
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-       });
+    $('#food').parent().append('<ul class="list-item" id="newfood" name="food"></ul>');
+    $('#food option').each(function(){
+        $('#newfood').append('<li value="' + $(this).val() + '">'+$(this).text()+'</li>');
+    });
+    $('#food').remove();
+    $('#newfood').attr('id', 'food');
+    $('#food li').first().addClass('init');
+    $("#food").on("click", ".init", function() {
+        $(this).closest("#food").children('li:not(.init)').toggle();
+    });
+    
+    var allOptions = $("#time").children('li:not(.init)');
+    $("#time").on("click", "li:not(.init)", function() {
+        allOptions.removeClass('selected');
+        $(this).addClass('selected');
+        $("#time").children('.init').html($(this).html());
+        allOptions.toggle();
     });
 
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-    
-
+    var FoodOptions = $("#food").children('li:not(.init)');
+    $("#food").on("click", "li:not(.init)", function() {
+        FoodOptions.removeClass('selected');
+        $(this).addClass('selected');
+        $("#food").children('.init').html($(this).html());
+        FoodOptions.toggle();
+    });
 })(jQuery);
